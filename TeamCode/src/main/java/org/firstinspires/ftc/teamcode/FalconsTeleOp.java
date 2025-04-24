3578c67bc568d45c8bf47ec14615a5a5905d88f2
Stephen <stephen@tinefamily.com>
2025-04-24 19:19:29 -0400

    Initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FalconsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FalconsTeleOp.java
@@ -4,15 +4,24 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class FalconsTeleOp extends LinearOpMode {
     //Initialize motors, servos, sensors, imus, etc.
-    DcMotorEx motorLF, motorRF, motorLB, motorRB;
-    // TODO: Uncomment the following line if you are using servos
-    //Servo Claw;
+    DcMotorEx backWheels;
+    Servo frontSteering;
 
-    public static MecanumDrive.Params DRIVE_PARAMS = new MecanumDrive.Params();
+    double currentSpeed = 0.0;
+    final double FORWARD_HEADING = 0.5;
+    double currentHeading = FORWARD_HEADING;
+
+    final double NORMAL_STEERING_MULTIPLIER = 0.3;
+    final double SLOW_STEERING_MULTIPLIER = 0.1;
+
+    boolean isCruising = false;
+    boolean lastDpadUp;
+    boolean lastDpadDown;
 
 
     // The following code will run as soon as "INIT" is pressed on the Driver Station
@@ -21,40 +30,21 @@ public class FalconsTeleOp extends LinearOpMode {
         //Define those motors and stuff
         //The string should be the name on the Driver Hub
         // Set the strings at the top of the MecanumDrive file; they are shared between TeleOp and Autonomous
-        motorLF = (DcMotorEx) hardwareMap.dcMotor.get(DRIVE_PARAMS.leftFrontDriveName);
-        motorLB = (DcMotorEx) hardwareMap.dcMotor.get(DRIVE_PARAMS.leftBackDriveName);
-        motorRF = (DcMotorEx) hardwareMap.dcMotor.get(DRIVE_PARAMS.rightFrontDriveName);
-        motorRB = (DcMotorEx) hardwareMap.dcMotor.get(DRIVE_PARAMS.rightBackDriveName);
+        backWheels = (DcMotorEx) hardwareMap.dcMotor.get("back_wheels");
 
         // Use the following line as a template for defining new servos
-        //Claw = (Servo) hardwareMap.servo.get("claw");
+        frontSteering = (Servo) hardwareMap.servo.get("front_wheels");
 
         //Set them to the correct modes
-        //This reverses the motor direction
-        // This data is also set at the top of MecanumDrive, for the same reasons as above
-        motorLF.setDirection(DRIVE_PARAMS.leftFrontDriveDirection);
-        motorLB.setDirection(DRIVE_PARAMS.leftBackDriveDirection);
-        motorRF.setDirection(DRIVE_PARAMS.rightFrontDriveDirection);
-        motorRB.setDirection(DRIVE_PARAMS.rightBackDriveDirection);
-
         //This resets the encoder values when the code is initialized
-        motorLF.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        motorLB.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        motorRF.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        motorRB.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backWheels.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
         //This makes the wheels tense up and stay in position when it is not moving, opposite is FLOAT
-        motorLF.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        motorLB.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        motorRF.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        motorRB.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backWheels.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
         //This lets you look at encoder values while the OpMode is active
         //If you have a STOP_AND_RESET_ENCODER, make sure to put this below it
-        motorLF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorLB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorRF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorRB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backWheels.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
         // The program will pause here until the Play icon is pressed on the Driver Station
@@ -63,38 +53,47 @@ public class FalconsTeleOp extends LinearOpMode {
         // opModeIsActive() returns "true" as long as the Stop button has not been pressed on the Driver Station
         while(opModeIsActive()) {
 
-            // Mecanum drive code
-            double powerX = 0.0;  // Desired power for strafing           (-1 to 1)
-            double powerY = 0.0;  // Desired power for forward/backward   (-1 to 1)
-            double powerAng = 0.0;  // Desired power for turning          (-1 to 1)
+            // Manual Drive code
+            if (!isCruising) {
+                if (gamepad1.left_trigger > 0) {
+                    // Deceleration controls
+                    currentSpeed = -gamepad1.left_trigger;
+                } else {
+                    // Acceleration controls
+                    currentSpeed = gamepad1.right_trigger;
+                }
+            }
 
-            // Set the desired powers based on joystick inputs (-1 to 1)
-            powerX = gamepad1.left_stick_x;
-            powerY = -gamepad1.left_stick_y;
-            powerAng = -gamepad1.right_stick_x;
+            if (Math.abs(gamepad1.right_stick_x) > 0.0) {
+                // Slow steering controls
+                currentHeading = FORWARD_HEADING + (gamepad1.right_stick_x * SLOW_STEERING_MULTIPLIER);
+            } else {
+                // Normal steering controls
+                currentHeading = FORWARD_HEADING + (gamepad1.left_stick_x * NORMAL_STEERING_MULTIPLIER);
+            }
 
-            // Perform vector math to determine the desired powers for each wheel
-            double powerLF = powerX + powerY - powerAng;
-            double powerLB = -powerX + powerY - powerAng;
-            double powerRF = -powerX + powerY + powerAng;
-            double powerRB = powerX + powerY + powerAng;
+            // Cruise Control code
+            if (gamepad1.dpad_right) {
+                isCruising = true;
+            } else if (gamepad1.dpad_left) {
+                isCruising = false;
+            }
 
-            // Determine the greatest wheel power and set it to max
-            double max = Math.max(1.0, Math.abs(powerLF));
-            max = Math.max(max, Math.abs(powerRF));
-            max = Math.max(max, Math.abs(powerLB));
-            max = Math.max(max, Math.abs(powerRB));
+            if (isCruising) {
+                if (gamepad1.dpad_up && !lastDpadUp) {
+                    // Increase cruising speed
+                    currentSpeed += 0.1;
+                } else if (gamepad1.dpad_down && !lastDpadDown) {
+                    // Decrease cruising speed
+                    currentSpeed -= 0.1;
+                }
+            }
+            lastDpadUp = gamepad1.dpad_up;
+            lastDpadDown = gamepad1.dpad_down;
 
-            // Scale all power variables down to a number between 0 and 1 (so that setPower will accept them)
-            powerLF /= max;
-            powerLB /= max;
-            powerRF /= max;
-            powerRB /= max;
-
-            motorLF.setPower(powerLF);
-            motorLB.setPower(powerLB);
-            motorRF.setPower(powerRF);
-            motorRB.setPower(powerRB);
+            // Set motor/servo powers/positions based on values
+            backWheels.setPower(currentSpeed);
+            frontSteering.setPosition(currentHeading);
 
 
 
